649f6bfd7dfc6811c391b4e847deef306edf3519
Gabriel Valdez <gvaldezm@gmail.com>
2017-03-10 22:54:06 -0800

    Add rules to show pingpong if number is divisible by 15, ping when it's divisible by 3, and pong when it's divisible by 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,32 @@
 $(document).ready(function() {
 
   $("form#pingpong").submit(function(event) {
+      //$("#result").hide();
       event.preventDefault();
       var numberEntered = $("#number").val();
       var numberEnteredParsed = parseInt(numberEntered);
+      var pingPongArray = [];
       //console.log(numberEnteredParsed);
       //console.log(numberEntered);
       //alert ("yes");
       var pingPongLoop = " ";
       for (var i=1; i <= numberEnteredParsed; i++){
-         pingPongLoop += i;
+         pingPongLoop += i + ", ";
          $("#result").text(pingPongLoop);
-         //alert (pingPongLoop);
+         pingPongArray = [pingPongLoop];
+         //alert [pingPongArray];
+         //console.log(pingPongLoop);
+         console.log(pingPongArray);
+
+         if (numberEnteredParsed % 15 === 0) {
+           $("#result").text("PingPong");
+         } else if (numberEnteredParsed % 5 === 0) {
+           $("#result").text("Pong");
+         } else if (numberEnteredParsed % 3 === 0) {
+           $("#result").text("Ping");
       }
+    }
+
     });
 });
 
