16cd7f5c7ebbedadc9e511862f72c3a73724e8c2
Gabriel Valdez <gvaldezm@gmail.com>
2017-03-19 10:00:04 -0700

    Fix issues where only one number would show at a time. Add page break so numbers show in a column rather in the same line. Add styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,36 @@
 $(document).ready(function() {
-
+ //$("#result").hide();
   $("form#pingpong").submit(function(event) {
       //$("#result").hide();
       event.preventDefault();
+      //$("#result").hide();
       var numberEntered = $("#number").val();
       var numberEnteredParsed = parseInt(numberEntered);
-      var pingPongArray = [];
-      //var dividers =
-      //console.log(numberEnteredParsed);
-      //console.log(numberEntered);
-      //alert ("yes");
-      var pingPongLoop = " ";
-      for (var i=1; i <= numberEnteredParsed; i++){
-         pingPongLoop += i + ", ";
-         $("#result").text(pingPongLoop);
-         pingPongArray = [pingPongLoop];
+      // var pingPongArray = [];
+      // var pingPongLoop = " ";
 
+      for (var i=1; i <= numberEnteredParsed; i++){
+         //pingPongLoop += i + ", ";
+         //$("#result").hide();
+         //$("#result").append(i + "</br>");
+         //pingPongArray = [pingPongLoop];
          //alert [pingPongArray];
          //console.log(pingPongLoop);
-         console.log(pingPongArray);
-         
-         if (numberEnteredParsed % 15 === 0) {
-           $("#result").text("PingPong");
-         } else if (numberEnteredParsed % 5 === 0) {
-           $("#result").text("Pong");
-         } else if (numberEnteredParsed % 3 === 0) {
-           $("#result").text("Ping");
-      }
+         console.log(i);
+
+         if (i % 15 === 0) {
+           $("#result").append("PingPong" + "</br>" );
+         } else if (i % 5 === 0) {
+           $("#result").append("Pong" + "</br>");
+         } else if (i % 3 === 0) {
+           $("#result").append("Ping"+ "</br>" );
+         } else {
+           $("#result").append(i + "</br>");
+         }
+
+        //  if (i === "Ping") {
+        //
+        //  }
     }
 
     });
